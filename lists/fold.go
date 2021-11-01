7542096af56e83e1179ef6252e4a897f3acbb041
Unix One <unix1@live.com>
2021-10-31 20:13:56 -0700

    Add lists fold and concurrent fold

---
+++ lists/fold.go
@@ -0,0 +1,49 @@
+package lists
+
+import (
+	"sync"
+)
+
+// Fold folds (aka reduces) a list of values to a single value using the provided function and the
+// starting accumulator.
+// It does so by calling the `fn` function on successive elements of the list. The `fn` function
+// receives the element of the list and the accumulator. It must return the new accumulator after
+// processing the element. The Fold function returns the final accumulator.
+func Fold[T any, Tacc any](fn func(elem T, acc Tacc) Tacc, acc0 Tacc, list []T) Tacc {
+	acc := acc0
+	for _, item := range list {
+		acc = fn(item, acc)
+	}
+	return acc
+}
+
+// FoldC is a concurrent version of Fold. It takes an additional concurrency argument that spreads
+// the `fn` calls over specified number of goroutines. If specified concurrency is greater than the
+// length of the list, the additional goroutines will not be started.
+// Keep in mind that concurrent multiple goroutines will concurrently access the accumulator.
+func FoldC[T any, Tacc any](fn func(elem T, acc *Tacc) *Tacc, acc *Tacc, list []T, concurrency int) *Tacc {
+	var wg sync.WaitGroup
+	if len(list) > concurrency {
+		concurrency = len(list)
+	}
+	ch := make(chan T, concurrency)
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			for {
+				e, ok := <-ch
+				if !ok {
+					wg.Done()
+					return
+				}
+				acc = fn(e, acc)
+			}
+		}()
+	}
+	for _, item := range list {
+		ch <- item
+	}
+	close(ch)
+	wg.Wait()
+	return acc
+}
---
+++ lists/fold_test.go
@@ -0,0 +1,132 @@
+package lists_test
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/unix1/gostdx/v1/lists"
+)
+
+type testCase[Telem any, Tacc any] struct {
+	name     string
+	list     []Telem
+	acc      Tacc
+	fn       func(Telem, Tacc) Tacc
+	expected Tacc
+}
+
+type testCaseC[Telem any, Tacc any] struct {
+	name     string
+	list     []Telem
+	acc      *Tacc
+	fn       func(Telem, *Tacc) *Tacc
+	expected *Tacc
+}
+
+type testCaseWithConcurrency[Telem any, Tacc any] struct {
+	testCase    testCaseC[Telem, Tacc]
+	concurrency int
+}
+
+func TestFold(t *testing.T) {
+	testCases := []testCase[int, int]{
+		{
+			"simple sum",
+			[]int{1, 2, 3},
+			0,
+			func(e int, acc int) int { return acc + e },
+			6,
+		},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			actual := lists.Fold(tt.fn, tt.acc, tt.list)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func newFoldSumTestCase(slow bool) testCaseC[int64, int64] {
+	return testCaseC[int64, int64]{
+		"simple sum",
+		[]int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		ptr(int64(0)),
+		func(e int64, acc *int64) *int64 {
+			if slow {
+				time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+			}
+			atomic.AddInt64(acc, e)
+			return acc
+		},
+		ptr(int64(55)),
+	}
+}
+
+func TestFoldC(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	testCases := []testCaseWithConcurrency[int64, int64]{
+		{newFoldSumTestCase(false), 1},
+		{newFoldSumTestCase(false), 3},
+		{newFoldSumTestCase(false), 10},
+		{newFoldSumTestCase(true), 3},
+		{newFoldSumTestCase(true), 10},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(fmt.Sprintf("%s (concurrency: %d)", tt.testCase.name, tt.concurrency), func(t *testing.T) {
+			actual := lists.FoldC(tt.testCase.fn, tt.testCase.acc, tt.testCase.list, tt.concurrency)
+			assert.Equal(t, tt.testCase.expected, actual)
+		})
+	}
+}
+
+type tuple struct {
+	v1 string
+	v2 string
+}
+
+type mapAcc struct {
+	sync.Mutex
+	m map[string]string
+}
+
+func newFoldTuplesToMapTestCase() testCaseC[tuple, mapAcc] {
+	return testCaseC[tuple, mapAcc]{
+		"simple fold tuples to map, concurrency 1",
+		[]tuple{{"k1", "v1"}, {"k2", "v2"}, {"k3", "v3"}, {"k4", "v4"}, {"k5", "v5"}, {"k6", "v6"}, {"k7", "v7"}, {"k8", "v8"}, {"k9", "v9"}, {"k10", "v10"}},
+		&mapAcc{m: map[string]string{}},
+		func(e tuple, acc *mapAcc) *mapAcc {
+			acc.Lock()
+			defer acc.Unlock()
+			acc.m[e.v1] = e.v2
+			return acc
+		},
+		&mapAcc{m: map[string]string{"k1": "v1", "k2": "v2", "k3": "v3", "k4": "v4", "k5": "v5", "k6": "v6", "k7": "v7", "k8": "v8", "k9": "v9", "k10": "v10"}},
+	}
+}
+
+func TestFoldCMap(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	testCases := []testCaseWithConcurrency[tuple, mapAcc]{
+		{newFoldTuplesToMapTestCase(), 1},
+		{newFoldTuplesToMapTestCase(), 3},
+		{newFoldTuplesToMapTestCase(), 10},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(fmt.Sprintf("%s (concurrency: %d)", tt.testCase.name, tt.concurrency), func(t *testing.T) {
+			actual := lists.FoldC(tt.testCase.fn, tt.testCase.acc, tt.testCase.list, tt.concurrency)
+			assert.Equal(t, tt.testCase.expected, actual)
+		})
+	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
